d0678542a1b28d4d68670c774638051030099de5
LennonWang <lennon@socrates.com>
2024-01-30 14:46:28 +0800

    b

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,6 @@ class HomePagState extends StatefulWidget {
 class _HomePagStateState extends State<HomePagState> {
   @override
   Widget build(BuildContext context) {
-    return  Placeholder();
+    return  const Placeholder();
   }
 }
