a446af3901fec40d9c41567e54a1130398a9cf1c
LennonWang <lennon@socrates.com>
2024-01-30 12:23:21 +0800

    添加Account Page

---
+++ lib/account_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountPagState extends StatefulWidget {
+  const AccountPagState({super.key});
+
+  @override
+  State<AccountPagState> createState() => _AccountPagStateState();
+}
+
+class _AccountPagStateState extends State<AccountPagState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
