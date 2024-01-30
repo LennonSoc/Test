5187d5ce829070a138461e2e72c99b104a352b8d
LennonWang <lennon@socrates.com>
2024-01-30 12:19:53 +0800

    添加pHome Page

---
+++ lib/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePagState extends StatefulWidget {
+  const HomePagState({super.key});
+
+  @override
+  State<HomePagState> createState() => _HomePagStateState();
+}
+
+class _HomePagStateState extends State<HomePagState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
